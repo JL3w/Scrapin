9f0478b1b34a8966863070ffde406955d51dc597
J Lew <jtl@metricwise.net>
2019-06-23 18:12:19 -0400

    add models

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.listen(PORT, function () {
+    console.log("Listening on port: " + PORT);
+});
